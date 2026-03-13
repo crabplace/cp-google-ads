f47c085b28c7666bab3a2d6b71105ce23d741a63
crabplace <greg@crabplace.com>
2026-03-13 09:35:55 -0400

    Implement GAQL query function for Google Ads API

---
+++ netlify/functions/netlify/functions/gaql.js
@@ -0,0 +1,25 @@
+const { getAccessToken } = require('./get-token');
+
+const CUSTOMER_ID = process.env.GOOGLE_ADS_CUSTOMER_ID;
+const DEV_TOKEN = process.env.GOOGLE_ADS_DEVELOPER_TOKEN;
+
+const queryGoogleAds = async (gaql) => {
+  const accessToken = await getAccessToken();
+  const resp = await fetch(
+    `https://googleads.googleapis.com/v18/customers/${CUSTOMER_ID}/googleAds:search`,
+    {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${accessToken}`,
+        'developer-token': DEV_TOKEN,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ query: gaql })
+    }
+  );
+  const data = await resp.json();
+  if (data.error) throw new Error(JSON.stringify(data.error));
+  return data.results || [];
+};
+
+module.exports = { queryGoogleAds };
